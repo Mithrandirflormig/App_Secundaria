9c2d3f44a1c5a82a5f3afaee3268d0784a099cf2
Mario A. Flormig <59883145+Mithrandirflormig@users.noreply.github.com>
2020-10-09 17:21:03 -0500

    Implementación MasterPage

---
+++ app_secundaria/App_secundaria/App_secundaria/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace App_secundaria
+{
+    using App_secundaria.Views;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+    {
+        #region Properties
+        public static NavigationPage Navigator { get; internal set; }
+        #endregion
+
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new LoginPage();
+        }
+        #endregion
+
+        #region Constructors
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        } 
+        #endregion
+    }
+}
