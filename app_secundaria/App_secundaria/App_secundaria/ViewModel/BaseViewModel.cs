a23d449b0cf8f679e0ce621f928cff8f35f5fe4d
Mario A. Flormig <59883145+Mithrandirflormig@users.noreply.github.com>
2020-10-11 01:04:21 -0500

    Refresco en campos password, IsEnabled e IsRuning

---
+++ app_secundaria/App_secundaria/App_secundaria/ViewModel/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace App_secundaria.ViewModel
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnpropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void Setvalue<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+            {
+                return;
+            }
+
+            backingField = value;
+            OnpropertyChanged(propertyName);
+        }
+    }
+}
--- app_secundaria/App_secundaria/App_secundaria/ViewModel/RegisterViewModel.cs
+++ app_secundaria/App_secundaria/App_secundaria/ViewModel/RegisterViewModel.cs
@@ -1,6 +1,132 @@
 ﻿namespace App_secundaria.ViewModel
 {
-    public class RegisterViewModel
+    using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class RegisterViewModel : BaseViewModel
     {
+
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+        #region Properties
+        public string Email 
+        { 
+            get; 
+            set; 
+        }
+
+        public string Password 
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                Setvalue(ref this.password, value);
+            }
+        }
+
+        public bool IsRemember 
+        { 
+            get; 
+            set; 
+        }
+
+        public bool IsRunning 
+        {
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                Setvalue(ref this.isRunning, value);
+            }
+        }
+
+        public bool IsEnabled 
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                Setvalue(ref this.isEnabled, value);
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand GoCommand 
+        { 
+            get
+            {
+                return new RelayCommand(Go);
+            }
+        }
+
+
+        private async void Go()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", 
+                    "You must enter an email", 
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Password",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "mario@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or Password Incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Perfect",
+                    "Let´s go!",
+                    "Accept");
+            return;
+        }
+        #endregion
+
+        #region Constructors
+        public RegisterViewModel()
+        {
+            this.IsRemember = true;
+            this.IsEnabled = true;
+        }
+
+        #endregion
     }
 }
